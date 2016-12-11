282bfe50785ad32235304b8b79abe90451ff60bb
Kevin Delisle <delisle@ca.ibm.com>
2016-12-11 02:50:48 -0500

    Add milestone generator for org-wide milestones
    Automatically generate milestones across all repositories in
    an organization or a user's account.

---
+++ test/test-milestone-generator.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Octokat = require('./fixtures/fake-octokat');
+var milestoneGenerator = require('../lib/milestone-generator');
+var tap = require('tap');
+
+tap.test('milestone-generator', function(t) {
+  var octo = new Octokat({
+    token: 'fake-token',
+  });
+  t.test('org', function(t) {
+    return milestoneGenerator({
+      octo: octo,
+      title: 'test-milestone',
+      due_on: '2016-12-26T00:00:00Z',
+      org: 'fake-org',
+      description: 'A test milestone made by strong-github-api',
+    }).then(function(result) {
+      t.ok(result, 'returned a thing');
+    });
+  });
+
+  t.test('user', function(t) {
+    return milestoneGenerator({
+      octo: octo,
+      title: 'test-milestone',
+      due_on: '2016-12-26T00:00:00Z',
+      user: 'fake-user',
+      description: 'A test milestone made by strong-github-api',
+    }).then(function(result) {
+      t.ok(result, 'returned a thing');
+    });
+  });
+
+  t.end();
+});
