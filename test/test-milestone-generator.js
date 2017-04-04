4e3d62f5fee2d050b9f8a1d92f7c2e5e9b711429
Kevin Delisle <delisle@ca.ibm.com>
2017-04-04 15:24:50 -0400

    milestone-generator: retrieve repos before run
    Attempt to fetch all of the repositories before run. This way, if
    any of them fail (don't exist, not allowed, etc), it won't run
    against the remainder of them.

--- test/test-milestone-generator.js
+++ test/test-milestone-generator.js
@@ -51,5 +51,33 @@ tap.test('milestone-generator', function(t) {
     });
   });
 
+  t.test('list fails on bogus repos', function(t) {
+    var octo = new Octokat({
+      token: 'fake-token',
+    });
+    // Repos will not be an array when called this way in Octokat!
+    octo.repoInfo = octo.fakeRepo;
+    octo.repos = function(org, name) {
+      return {
+        fetch: function() {
+          return name === octo.repoInfo.name ?
+            Promise.resolve(octo.fakeRepo) :
+            Promise.reject(new Error('Not Found'));
+        },
+      };
+    };
+    return milestoneGenerator({
+      octo: octo,
+      title: 'test-milestone',
+      dueOn: '2016-12-26T00:00:00Z',
+      user: 'fake-user',
+      description: 'A test milestone made by strong-github-api',
+      list: path.resolve('test', 'fixtures', 'fake-repos-bad.json'),
+    }).catch(function(err) {
+      t.ok(err, 'returned error');
+      t.contains(err.message, 'Not Found', 'message was correct');
+    });
+  });
+
   t.end();
 });
