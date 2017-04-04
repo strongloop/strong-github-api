c0305ae92e235c7f4fd5e58fe7db7b14de41e627
Kevin Delisle <delisle@ca.ibm.com>
2017-04-04 13:30:39 -0400

    milestone-generator: allow repository list
    Provide a list with the --list flag, which will avoid scanning
    the target org/user repo list, and only use the specified
    collection of repos.

--- test/test-milestone-generator.js
+++ test/test-milestone-generator.js
@@ -3,6 +3,7 @@
 var Octokat = require('./fixtures/fake-octokat');
 var milestoneGenerator = require('../index').milestoneGenerator;
 var tap = require('tap');
+var path = require('path');
 
 tap.test('milestone-generator', function(t) {
   var octo = new Octokat({
@@ -27,6 +28,24 @@ tap.test('milestone-generator', function(t) {
       dueOn: '2016-12-26T00:00:00Z',
       user: 'fake-user',
       description: 'A test milestone made by strong-github-api',
+    }).then(function(result) {
+      t.ok(result, 'returned a thing');
+    });
+  });
+
+  t.test('list', function(t) {
+    var octo = new Octokat({
+      token: 'fake-token',
+    });
+    // Repos will not be an array when called this way in Octokat!
+    octo.repoInfo = octo.fakeRepo;
+    return milestoneGenerator({
+      octo: octo,
+      title: 'test-milestone',
+      dueOn: '2016-12-26T00:00:00Z',
+      user: 'fake-user',
+      description: 'A test milestone made by strong-github-api',
+      list: path.resolve('test', 'fixtures', 'fake-repos.json'),
     }).then(function(result) {
       t.ok(result, 'returned a thing');
     });
